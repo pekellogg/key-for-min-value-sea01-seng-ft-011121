79dd016218c55cda8420b05b2f7719b167d35026
Peyton Kellogg <pekellogg@hotmail.com>
2021-01-04 01:56:31 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,10 +3,11 @@
 
 def key_for_min_value(name_hash)
   min_value = [key][value]
+
   name_hash.each do |key, value|
     if min_value < value
       min_value = value
     end
-  # puts :key
   end # else nil
+  puts :key
 end
