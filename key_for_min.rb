adeb7118e1751a830b8969e2f12176b934dda9f2
Peyton Kellogg <pekellogg@hotmail.com>
2021-01-04 01:40:34 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,10 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  hash.each |key, value|
-    min_value = value
+  hash.each {|key, value|
+  min_value = value
     if min_value < value
       min_value = value
       puts :key
+    end
+  }
+
     else nil
 end
